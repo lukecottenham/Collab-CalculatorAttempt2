2d9a5b2c9827bedc4aaf95031eb5f4cfdb474c4d
lukecottenham <luke.cottenham@hotmail.co.uk>
2019-12-03 13:28:43 +0000

    Third Commit

--- src/main/java/com/bae/calc/DivideCalculator.java
+++ src/main/java/com/bae/calc/DivideCalculator.java
@@ -1,4 +1,4 @@
-package com.bae.app;
+package com.bae.calc;
 
 public class DivideCalculator {
 
--- src/test/java/com/bae/divideTest/DivideTest.java
+++ src/test/java/com/bae/divideTest/DivideTest.java
@@ -4,7 +4,7 @@ package com.bae.divideTest;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.bae.app.DivideCalculator;
+import com.bae.calc.DivideCalculator;
 
 public class DivideTest {
 
