49b3dca36589fadceb0694cceaa47fe7c6a87889
chrisreesjacobs@gmail.com <urC0..tPFObM>
2019-12-03 12:33:09 +0000

    Test Red of exponential function
    Signed-off-by: chrisreesjacobs@gmail.com <urC0..tPFObM>

--- src/main/java/com/bae/calc/Exponential.java
+++ src/main/java/com/bae/calc/Exponential.java
@@ -1,9 +1,9 @@
 package com.bae.calc;
 
 public class Exponential extends Calculator{
-    public static String exponential(String x_to_the_a) {
+    public static void exponential(String x_to_the_a) {
     }
 
-    public static String exponential(int i, int i1) {
+    public static void exponential(int i, int i1) {
     }
 }
--- src/test/java/com/bae/calc/ExponentialTest.java
+++ src/test/java/com/bae/calc/ExponentialTest.java
@@ -3,7 +3,7 @@ package com.bae.calc;
 import org.junit.Assert;
 import org.junit.Test;
 
-class ExponentialTest {
+public class ExponentialTest {
     @Test
     public void testExponential() throws Exception {
         Exponential exponential = new Exponential();
