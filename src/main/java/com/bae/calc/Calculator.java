0d361a5b94e362b618e41daef43d4cb07ac826c1
lukecottenham <luke.cottenham@hotmail.co.uk>
2019-12-03 12:44:10 +0000

    First Commit

---
+++ src/main/java/com/bae/calc/Calculator.java
@@ -0,0 +1,5 @@
+package com.bae.calc;
+
+public class Calculator {
+
+}
---
+++ src/test/java/com/bae/divideTest/DivideTest.java
@@ -0,0 +1,14 @@
+package com.bae.divideTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DivideTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
